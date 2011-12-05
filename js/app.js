7771ed06caf91e16344224b2f1da2f4364abd0cb
James Aitken <loonypandora@gmail.com>
2011-12-05 14:03:41 +0000

    We will need masonry when the blog posts return

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 
+// No JS until the blog comes along
+
 $(document).ready(function(){            
 
     // Vertical rhythm and layout grid
-    var grid = new hashgrid();
+    // var grid = new hashgrid();
+
+    // $('.container').masonry();
 
 });
 
