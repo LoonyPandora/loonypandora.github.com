587da82c3c73ab9f598075d36932cd55d7ba992f
James Aitken <loonypandora@gmail.com>
2014-03-17 22:26:35 +0000

    Add font awesome and the jQuery plugin

--- js/site.js
+++ js/site.js
@@ -13,9 +13,11 @@ $(document).ready(function () {
     });
 
     getTopArtists();
+    getRecentTracks();
     getFollowers();
     getRepos();
     getFeed();
+    startWordClock();
 });
 
 
@@ -52,6 +54,61 @@ function getTopArtists () {
         });
 
         $(".top-artists").addClass("animated fadeInDown");
+    });
+
+
+    request.fail(function () {
+        
+    });
+
+
+};
+
+
+
+function getRecentTracks () {
+    var recentTracksURL = "http://ws.audioscrobbler.com" +
+                          "/2.0" + 
+                          "/?method=user.getrecenttracks" + 
+                          "&user=LoonyPandora" + 
+                          "&format=json" + 
+                          "&limit=5" + 
+                          "&api_key=c1e48cba96f019d566438bd5e7f1591f";
+
+    var request = $.ajax({
+        dataType: "json",
+        url: recentTracksURL,
+        type: "GET"
+    });
+
+    request.done(function (response) {
+        var $template = $("#recent-tracks-template").html();
+
+        $.each(response.recenttracks.track, function (i, track) {
+            // Check nowplaying / date played status
+            // API will return double for an item that is now playing. So skip if it is
+            var date;
+            if (track.date) {
+                date = track.date["#text"];
+            } else {
+                return true;
+            }
+
+            var renderData = {
+                url: track.url,
+                artist: track.artist["#text"],
+                track: track.name,
+                album: track.album["#text"],
+                date: date,
+                img: track.image[1]["#text"],
+            };
+
+            $(".recent-tracks").append(
+                Mustache.render($template, renderData)
+            );
+        });
+
+        $(".recent-tracks").addClass("animated fadeInDown");
     });
 
 
@@ -158,3 +215,12 @@ function getFeed () {
     })
 }
 
+
+
+
+function startWordClock () {
+    $(".wordclock").wordclock({
+        quanta : ["hours", "minutes", "seconds"]
+    });
+    
+}
