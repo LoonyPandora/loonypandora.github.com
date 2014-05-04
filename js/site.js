afa046dd9f5c0b1a0055259ec1bbf55224f15931
James Aitken <loonypandora@gmail.com>
2014-05-04 04:52:21 +0100

    Finalise layout

--- js/site.js
+++ js/site.js
@@ -1,11 +1,73 @@
+"use strict";
+
+$(document).ready(function () {
+    $("body").click(function () {
+        $(".recent-tracks li").removeClass("selected");
+    });
+
+    getRecentTracks(8);
+    
+    repaintOnResize("h1, a, img, section");
+});
 
 
+
+
+function getRecentTracks (limit) {
+    var recentTracksURL = "http://ws.audioscrobbler.com" +
+                          "/2.0" + 
+                          "/?method=user.getrecenttracks" + 
+                          "&user=LoonyPandora" + 
+                          "&format=json" + 
+                          "&limit=" + limit + 
+                          "&api_key=c1e48cba96f019d566438bd5e7f1591f";
+
+    var request = $.ajax({
+        dataType: "json",
+        url: recentTracksURL,
+        type: "GET"
+    });
+
+    request.done(function (response) {
+        var $template = $("#recent-tracks-template").html();
+
+        $.each(response.recenttracks.track, function (i, track) {
+            // Check nowplaying / date played status
+            // API will return double for an item that is now playing. So skip if it is
+            var date;
+            if (track.date) {
+                date = track.date["#text"];
+            } else {
+                return true;
+            }
+
+            var renderData = {
+                url: track.url,
+                artist: track.artist["#text"],
+                track: track.name,
+                album: track.album["#text"],
+                date: date,
+                img: track.image[2]["#text"],
+            };
+
+            $(".recent-tracks").append(
+                Mustache.render($template, renderData)
+            );
+        });
+
+        $(".recent-tracks").addClass("animated fadeIn");
+    });
+
+    request.fail(function () {
+        
+    });
+};
 
 
 
 // Workaround Safari issue where elements are not repainted
-causeRepaintsOn = $("h1, a, img, section");
-
-$(window).resize(function() {
-    causeRepaintsOn.css("z-index", 1);
-});
+function repaintOnResize (selector) {
+    $(window).resize(function() {
+        $(selector).css("z-index", 1);
+    });
+}
