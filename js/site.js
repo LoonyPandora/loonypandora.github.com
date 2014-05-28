a0d133e742cac88f9315b302a5b2ffc0aa510a6e
James Aitken <loonypandora@gmail.com>
2014-05-28 22:25:28 +0100

    Add proper handlers for showing track metadata

--- js/site.js
+++ js/site.js
@@ -13,6 +13,9 @@ $(document).ready(function () {
 
 
     var grid = new hashgrid();
+
+    setupTrackClickHandler();
+
 });
 
 
@@ -94,69 +97,13 @@ function getRecentTracks (limit) {
         type: "GET"
     });
 
+    // Template for showing an individual track
+    var $template = $("#recent-tracks-template").html();
+
     request.done(function (response) {
-        var $template = $("#recent-tracks-template").html();
-
-        var renderData = [];
-
         $.each(response.recenttracks.track.reverse(), function (i, track) {
-            // Check nowplaying / date played status
-            // API will return double for an item that is now playing. So skip if it is
-            var date;
-            if (track.date) {
-                date = track.date["#text"];
-            } else {
-                return true;
-            }
-
-            var momentDate = moment.utc(date, "DD MMM YYYY, HH:mm");
-
-            renderData.push({
-                url: track.url,
-                artist: track.artist["#text"],
-                track: track.name,
-                humanDate: momentDate.format("ddd, MMM Do YYYY, h:mm a"),
-                utcDate: momentDate.toISOString(),
-                timeAgo: momentDate.fromNow(),
-                thumbnail: track.image[2]["#text"],
-                img: track.image[3]["#text"],
-            });
-
-            $(".recent-tracks").append(
-                Mustache.render($template, renderData[i])
-            );
+            renderTrack(track, $template);
         });
-
-        $(".recent-tracks li a").on("click", function () {
-            var $meta = $("#recent-tracks-metadata").html();
-
-            var momentDate = moment.utc($(this).attr("datetime"));
-
-            $(".recent.metadata").remove();
-
-            $(".recent-section").append(
-                Mustache.render($meta, {
-                    artist: $(this).data("artist"),
-                    track: $(this).data("track"),
-                    img: $(this).data("img") || $(this).data("thumbnail"),
-                    timeAgo: momentDate.fromNow()
-                })
-            );
-
-            // $(".recent-tracks li.selected").remove();
-            // $(this).parent("li").clone().addClass("selected").appendTo(".recent-tracks");
-        });
-
-        // $(".recent-tracks li.selected a").on("click", function () {
-        //     console.log(this);
-        //     $(this).parent("li").removeClass("selected");
-        // });
-
-        // Add the metadata about the currently playing track and show it
-        // $(".recent-tracks").append(
-        //     Mustache.render($("#recent-tracks-metadata").html(), renderData.slice(-1)[0])
-        // ).addClass("animated fadeIn");
-
     });
 
     request.fail(function () {
@@ -164,6 +111,81 @@ function getRecentTracks (limit) {
     });
 };
 
+
+function renderTrack (track, $template) {
+    // Check nowplaying / date played status
+    // API will return double for an item that is now playing. So skip if it is
+    var date;
+    if (track.date) {
+        date = track.date["#text"];
+    } else {
+        return true;
+    }
+
+    var momentDate = moment.utc(date, "DD MMM YYYY, HH:mm");
+
+    $(".recent-tracks").append(
+        Mustache.render($template, {
+            url: track.url,
+            artist: track.artist["#text"],
+            track: track.name,
+            humanDate: momentDate.format("ddd, MMM Do YYYY, h:mm a"),
+            utcDate: momentDate.toISOString(),
+            timeAgo: momentDate.fromNow(),
+            thumbnail: track.image[2]["#text"],
+            img: track.image[3]["#text"],
+        })
+    ).addClass("animated fadeIn");
+}
+
+
+function setupTrackClickHandler () {
+    // Add the handler for showing / hiding metadata
+    $(document).on("click", function(event) {
+        var $target = $(event.target);
+
+        // If we've clicked inside the recent tracks section...
+        if ($target.parents().hasClass("recent-section")) {
+            // If click was on the metadata block remove it
+            if ($target.hasClass("recent metadata")) {
+                hideTrackMetadata();
+            }
+
+            // Check if it's a track we've clicked on.
+            if ($target.parents().hasClass("recent-tracks")) {
+                showTrackMetadata($target);
+            }
+        } else {
+            // Remove the metadata if the click was anywhere else
+            hideTrackMetadata();
+        }
+    });
+}
+
+
+function hideTrackMetadata () {
+    $(".recent.metadata").remove();
+}
+
+function showTrackMetadata ($track) {
+    var $meta = $("#recent-tracks-metadata").html();
+
+    var momentDate = moment.utc($track.attr("datetime"));
+
+    // Remove any other metadata viewing blocks
+    $(".recent.metadata").remove();
+
+    $(".recent-section").append(
+        Mustache.render($meta, {
+            artist:     $track.data("artist"),
+            track:      $track.data("track"),
+            img:        $track.data("img") || $track.data("thumbnail"),
+            timeAgo:    momentDate.fromNow()
+        })
+    );
+
+    $(".recent.metadata").addClass("animated pulse");
+}
 
 
 // Workaround Safari issue where elements are not repainted
